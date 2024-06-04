10760ee23760bed44bfc2dcce70eba57f94ffe3a
Michel Rodriguez <danielam.rodriguez.97@gmail.com>
2024-06-04 02:40:43 -0600

    calculadora

---
+++ practicas/calculadora/CalculadoraMichel.java
@@ -0,0 +1,26 @@
+package practicas.calculadora;
+
+public class CalculadoraMichel {
+    public static void main(String args []){
+        //Variables donde haré el calculo.
+        int numero1 = 30;
+        int numero2 = 50;      
+        
+        // Suma
+        int suma = numero1 + numero2;
+        System.out.println("El valor de la suma "+suma);
+
+        //Resta
+        int resta = numero1 - numero2;
+        System.out.println("El valor de la resta es : " +resta);
+
+        //Multiplicación
+        int multiplicación = numero1 * numero2;
+        System.out.println("El valor de la multiplicación es : " +multiplicación);
+
+        //División
+        int División = numero1 / numero2;
+        System.out.println("El valor de la división es :" +División);
+
+    }
+}   
